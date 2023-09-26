1bd2b97f8cc2d7933eac343e96e2f6cda62ee92b
DQHdeptrai2 <92821549+DQHdeptrai2@users.noreply.github.com>
2023-09-26 22:54:09 +0700

    Add files via upload

---
+++ network_monitor_gui.py
@@ -0,0 +1,132 @@
+import tkinter as tk
+from tkinter import ttk
+from scapy.all import *
+import numpy as np
+import time
+import os 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# Tạo cửa sổ giao diện người dùng
+window = tk.Tk()
+window.title("Network Monitor")
+
+# Hàm để chặn IP sử dụng iptables
+def ban_ip(ip, duration=600):
+    os.system(f'iptables -A INPUT -s {ip} -j DROP')
+    time.sleep(duration)
+    os.system(f'iptables -D INPUT -s {ip} -j DROP')
+
+# Mô hình học máy
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+
+# Dữ liệu để huấn luyện mô hình
+training_data = []
+
+# Hàm xử lý gói tin
+def packet_callback(packet):
+    protocol = packet.name.lower()
+    if protocol in packet_types:
+        timestamps.append(packet.time)
+        if len(timestamps) > thresholds[protocol]:
+            # Tính đặc trưng thời gian và thống kê mạng
+            time_features = compute_time_features(timestamps)
+            network_features = compute_network_features(packet)
+
+            # Kết hợp các đặc trưng lại
+            features = np.concatenate((time_features, network_features), axis=None)
+
+            log_message = f'{protocol} traffic ({len(timestamps)} {protocol} packets in {window_size} seconds)'
+            log_text.config(state=tk.NORMAL)
+            log_text.insert(tk.END, log_message + "\n")
+            log_text.config(state=tk.DISABLED)
+
+            # Phân tích lưu lượng mạng bất thường bằng mô hình Random Forest
+            is_abnormal = model.predict([features])[0]
+
+            if is_abnormal:
+                log_text.config(state=tk.NORMAL)
+                log_text.insert(tk.END, 'Detected abnormal network traffic\n')
+                log_text.config(state=tk.DISABLED)
+
+                # Chặn IP nguồn và ghi nhật ký
+                scapy.send(IP(src=packet.dst, dst=packet.src)/ICMP(type=ICMP.ECHO_REPLY)/packet.payload)
+                attacking_ip_addresses.add(packet.src)
+                with open('icmp_flood.log', 'a') as f:
+                    f.write(str(packet) + '\n')
+                ban_ip(packet.src)
+
+        if packet.src in attacking_ip_addresses:
+            scapy.send(packet, ack=0, ttl=0)
+
+# Tính đặc trưng thời gian
+def compute_time_features(timestamps):
+    timestamps = np.array(timestamps)
+    time_diff = np.diff(timestamps)
+    return [len(timestamps), time_diff.mean(), time_diff.std()]
+
+# Tính đặc trưng thống kê mạng
+def compute_network_features(packet):
+    # Tính toán các đặc trưng thống kê từ gói tin mạng
+    # Đây là ví dụ, bạn có thể sử dụng các đặc trưng phù hợp với vấn đề cụ thể của bạn.
+    return [len(packet), packet.len, packet.ttl]
+
+# Callback function when the "Start Monitoring" button is clicked
+def start_monitoring():
+    global packet_types, thresholds, window_size, timestamps, attacking_ip_addresses
+    packet_types = ['tcp', 'udp', 'arp', 'dns', 'http', 'https']
+    thresholds = {
+        'tcp': 1,
+        'udp': 1,
+        'arp': 1,
+        'dns': 1,
+        'http': 1,
+        'https': 1
+    }
+    window_size = int(window_size_entry.get())
+    timestamps = []
+    attacking_ip_addresses = set()
+
+    log_text.config(state=tk.NORMAL)
+    log_text.delete(1.0, tk.END)
+    log_text.config(state=tk.DISABLED)
+
+    try:
+        log_text.config(state=tk.NORMAL)
+        log_text.insert(tk.END, 'Starting to monitor traffic...\n')
+        log_text.config(state=tk.DISABLED)
+
+        # Sử dụng scapy để bắt gói tin mạng
+        sniff(filter='tcp, udp, arp, dns, icmp', prn=packet_callback)
+    except KeyboardInterrupt:
+        log_text.config(state=tk.NORMAL)
+        log_text.insert(tk.END, 'Ending program...\n')
+        log_text.config(state=tk.DISABLED)
+
+# Create GUI elements
+label_packet_types = tk.Label(window, text="Packet types to monitor (comma-separated):")
+packet_types_entry = tk.Entry(window)
+label_window_size = tk.Label(window, text="Window size (s):")
+window_size_entry = tk.Entry(window)
+start_button = tk.Button(window, text="Start Monitoring", command=start_monitoring)
+log_text = tk.Text(window, height=10, width=50)
+log_text.config(state=tk.DISABLED)
+
+# Grid layout for GUI elements
+label_packet_types.grid(row=0, column=0, sticky="w")
+packet_types_entry.grid(row=0, column=1)
+label_window_size.grid(row=1, column=0, sticky="w")
+window_size_entry.grid(row=1, column=1)
+start_button.grid(row=2, columnspan=2)
+log_text.grid(row=3, columnspan=2)
+
+# Initialize global variables
+packet_types = []
+thresholds = {}
+window_size = 0
+timestamps = []
+attacking_ip_addresses = set()
+
+# Start the GUI main loop
+window.mainloop()
