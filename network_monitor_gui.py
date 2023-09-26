66aa65457ed7e401bc4b5246887bd5a7f39a26b5
DQHdeptrai2 <92821549+DQHdeptrai2@users.noreply.github.com>
2023-09-27 00:46:03 +0700

    Update network_monitor_gui.py

--- network_monitor_gui.py
+++ network_monitor_gui.py
@@ -1,158 +1,175 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 from scapy.all import *
 import numpy as np
-import time
-import os 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+import os
+import sqlite3
+import re
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from threading import Thread
 
 # Create a Tkinter window
 window = tk.Tk()
 window.title("Security Monitoring System")
 
-# Create a StringVar to store the default value
-default_value = tk.StringVar()
-default_value.set("50")
+# Create a Text widget to display the log
+log_text_widget = tk.Text(window, height=10, width=50)
+log_text_widget.config(state=tk.DISABLED)
+log_text_widget.pack()
 
-# Create an Entry widget and link it to the StringVar
-window_size_entry = tk.Entry(window, width=50, textvariable=default_value)
+# Function to update log text
+def update_log_text(message):
+    log_text_widget.config(state=tk.NORMAL)
+    log_text_widget.insert(tk.END, message + '\n')
+    log_text_widget.config(state=tk.DISABLED)
 
-# Pack the Entry widget to display it
-window_size_entry.pack()
+# Create a variable to track real-time monitoring state
+real_time_monitoring_enabled = tk.BooleanVar()
+real_time_monitoring_enabled.set(False)
 
-# Start the Tkinter main loop
-window.mainloop()
+# Function to toggle real-time monitoring
+def toggle_real_time_monitoring():
+    global real_time_monitoring_thread
+    if real_time_monitoring_enabled.get():
+        update_log_text('Starting traffic monitoring...')
+        real_time_monitoring_thread = Thread(target=start_monitoring)
+        real_time_monitoring_thread.start()
+    else:
+        update_log_text('Stopping traffic monitoring...')
 
-# Hàm để chặn IP sử dụng iptables
-def ban_ip(ip, duration=600):
-    os.system(f'iptables -A INPUT -s {ip} -j DROP')
-    time.sleep(duration)
-    os.system(f'iptables -D INPUT -s {ip} -j DROP')
+# Checkbutton to toggle real-time monitoring
+real_time_monitoring_checkbox = tk.Checkbutton(window, text="Enable Real-time Monitoring", variable=real_time_monitoring_enabled, command=toggle_real_time_monitoring)
+real_time_monitoring_checkbox.pack()
 
-# Mô hình học máy
-model = RandomForestClassifier(n_estimators=100, random_state=0)
+# Create a button to clear the log
+clear_log_button = tk.Button(window, text="Clear Log", command=lambda: update_log_text('Log cleared.'))
+clear_log_button.pack()
 
-# Dữ liệu để huấn luyện mô hình
-training_data = []
+# Create a menu bar
+menubar = tk.Menu(window)
+window.config(menu=menubar)
 
-# Hàm xử lý gói tin
+# Create a "File" menu
+file_menu = tk.Menu(menubar, tearoff=0)
+menubar.add_cascade(label="File", menu=file_menu)
+
+# Function to export log
+def export_log():
+    filename = filedialog.asksaveasfilename(title="Export Log", defaultextension=".txt")
+    if filename:
+        with open(filename, "w") as f:
+            log_contents = log_text_widget.get("1.0", tk.END)
+            f.write(log_contents)
+
+# Add the "Export Log" option to the "File" menu
+file_menu.add_command(label="Export Log", command=export_log)
+
+# Create an Entry widget for entering packet types to monitor
+packet_types_label = tk.Label(window, text="Enter packet types to monitor")
+packet_types_label.pack()
+
+packet_types_entry = tk.Entry(window, width=50)
+packet_types_entry.pack()
+
+# Create a button to train a new machine learning model
+def train_new_model():
+    # Your code to train a new machine learning model goes here
+    pass
+
+train_model_button = tk.Button(window, text="Train New Machine Learning Model", command=train_new_model)
+train_model_button.pack()
+
+# Create a Scale widget to adjust the sensitivity of the intrusion detection algorithm
+sensitivity_label = tk.Label(window, text="Adjust Intrusion Detection Sensitivity")
+sensitivity_label.pack()
+
+sensitivity_slider = tk.Scale(window, from_=0, to=100, orient="horizontal")
+sensitivity_slider.pack()
+
+# ... (Thresholds, window size, timestamps, attacking_ip_addresses, database creation, and model initialization)
+
+# Define a function to calculate advanced features
+def calculate_advanced_features(packet):
+    # Calculate advanced features such as entropy, port distribution, etc.
+    # Example: entropy_feature = calculate_entropy(packet.payload)
+    # Return a list of advanced features
+    return []
+
+# Define a function to detect DDoS attacks
+def detect_ddos(packet):
+    # Implement DDoS detection logic here
+    # Example: Check for a sudden increase in traffic from multiple sources
+    return False
+
+# Modify the packet callback function to use advanced features and DDoS detection
 def packet_callback(packet):
-    protocol = packet.name.lower()
-    if protocol in packet_types:
-        timestamps.append(packet.time)
-        if len(timestamps) > thresholds[protocol]:
-            # Tính đặc trưng thời gian và thống kê mạng
-            time_features = compute_time_features(timestamps)
-            network_features = compute_network_features(packet)
+    global timestamps, attacking_ip_addresses
 
-            # Kết hợp các đặc trưng lại
-            features = np.concatenate((time_features, network_features), axis=None)
+    # Check if the packet type is in the list of monitored types
+    if packet.type not in packet_types:
+        return
 
-            log_message = f'{protocol} traffic ({len(timestamps)} {protocol} packets in {window_size} seconds)'
-            log_text.config(state=tk.NORMAL)
-            log_text.insert(tk.END, log_message + "\n")
-            log_text.config(state=tk.DISABLED)
+    # Update timestamps
+    timestamps.append(packet.time)
 
-            # Phân tích lưu lượng mạng bất thường bằng mô hình Random Forest
-            is_abnormal = model.predict([features])[0]
+    # Calculate statistical and temporal features
+    features = compute_features(packet)
+    
+    # Calculate advanced features
+    advanced_features = calculate_advanced_features(packet)
+    
+    # Merge the feature vectors
+    features += advanced_features
 
-            if is_abnormal:
-                log_text.config(state=tk.NORMAL)
-                log_text.insert(tk.END, 'Detected abnormal network traffic\n')
-                log_text.config(state=tk.DISABLED)
+    # Get the packet type
+    packet_type = get_packet_type(packet)
 
-                # Chặn IP nguồn và ghi nhật ký
-                scapy.send(IP(src=packet.dst, dst=packet.src)/ICMP(type=ICMP.ECHO_REPLY)/packet.payload)
-                attacking_ip_addresses.add(packet.src)
-                with open('icmp_flood.log', 'a') as f:
-                    f.write(str(packet) + '\n')
-                ban_ip(packet.src)
+    # Detect DDoS attacks
+    if detect_ddos(packet):
+        # Log the DDoS attack
+        log_message = 'Possible DDoS attack from {}: {} ({})'.format(packet.src, packet.summary(), packet_type)
+        update_log_text(log_message)
+        # conn.execute('INSERT INTO logs (message) VALUES (?)', (log_message,))
+        # conn.commit()
+    
+    # Make predictions using the machine learning model
+    prediction = model.predict(np.array([features]))
 
-        if packet.src in attacking_ip_addresses:
-            scapy.send(packet, ack=0, ttl=0)
+    # If the prediction indicates an anomaly, log it
+    if prediction > 0.5:  # Adjust the threshold as needed
+        # Add a field to the log message to store the packet type
+        log_message = 'Anomalous packet from {}: {} ({})'.format(packet.src, packet.summary(), packet_type)
+        update_log_text(log_message)
+        # conn.execute('INSERT INTO logs (message) VALUES (?)', (log_message,))
+        # conn.commit()
 
-# Tính đặc trưng thời gian
-def compute_time_features(timestamps):
-    timestamps = np.array(timestamps)
-    time_diff = np.diff(timestamps)
-    return [len(timestamps), time_diff.mean(), time_diff.std()]
-
-# Tính đặc trưng thống kê mạng
-def compute_network_features(packet):
-    # Tính toán các đặc trưng thống kê từ gói tin mạng
-    # Đây là ví dụ, bạn có thể sử dụng các đặc trưng phù hợp với vấn đề cụ thể của bạn.
-    return [len(packet), packet.len, packet.ttl]
-
-# Callback function when the "Start Monitoring" button is clicked
+# Modify the start_monitoring function to use packet_callback
 def start_monitoring():
-    global packet_types, thresholds, window_size, timestamps, attacking_ip_addresses
-    packet_types = ['tcp', 'udp', 'arp', 'dns', 'http', 'https']
-    thresholds = {
-        'tcp': 1,
-        'udp': 1,
-        'arp': 1,
-        'dns': 1,
-        'http': 1,
-        'https': 1
-    }
-    window_size = int(window_size_entry.get())
-    timestamps = []
-    attacking_ip_addresses = set()
+    global real_time_monitoring_enabled
 
-    log_text.config(state=tk.NORMAL)
-    log_text.delete(1.0, tk.END)
-    log_text.config(state=tk.DISABLED)
+    # Check if real-time monitoring is enabled
+    if not real_time_monitoring_enabled.get():
+        return
 
     try:
-        log_text.config(state=tk.NORMAL)
-        log_text.insert(tk.END, 'Starting to monitor traffic...\n')
-        log_text.config(state=tk.DISABLED)
-
-        # Sử dụng scapy để bắt gói tin mạng
+        update_log_text('Starting traffic monitoring...')
+        # Sniff packets and use the packet_callback function
         sniff(filter='tcp, udp, arp, dns, icmp', prn=packet_callback)
     except KeyboardInterrupt:
-        log_text.config(state=tk.NORMAL)
-        log_text.insert(tk.END, 'Ending program...\n')
-        log_text.config(state=tk.DISABLED)
+        update_log_text('Program terminated...')
 
-# Create GUI elements
-label_packet_types = tk.Label(window, text="Packet types to monitor (comma-separated):")
-packet_types_entry = tk.Entry(window)
-label_window_size = tk.Label(window, text="Window size (s):")
-window_size_entry = tk.Entry(window)
-start_button = tk.Button(window, text="Start Monitoring", command=start_monitoring)
-log_text = tk.Text(window, height=10, width=50)
-log_text.config(state=tk.DISABLED)
+# Bind the clear log button to a function to clear the log
+def clear_log():
+    log_text_widget.config(state=tk.NORMAL)
+    log_text_widget.delete('1.0', tk.END)
+    log_text_widget.config(state=tk.DISABLED)
 
-# Grid layout for GUI elements
-label_packet_types.grid(row=0, column=0, sticky="w")
-packet_types_entry.grid(row=0, column=1)
-label_window_size.grid(row=1, column=0, sticky="w")
-window_size_entry.grid(row=1, column=1)
-start_button.grid(row=2, columnspan=2)
-log_text.grid(row=3, columnspan=2)
+clear_log_button.config(command=clear_log)
 
-# Define packet types to monitor
-packet_types = ['tcp', 'udp', 'arp', 'dns', 'http', 'https']
+# Bind the train model button to a function to train a new machine learning model
+train_model_button.config(command=train_new_model)
 
-# Set thresholds for each packet type
-thresholds = {
-    'tcp': 1000,
-    'udp': 1000,
-    'arp': 1000,
-    'dns': 1000,
-    'http': 1000,
-    'https': 1000
-}
-
-# Set window size in seconds
-window_size = 50
-
-# Initialize timestamps and attacking IP addresses
-timestamps = []
-attacking_ip_addresses = set()
-
-# Start the GUI main loop
+# Start the Tkinter main loop
 window.mainloop()
